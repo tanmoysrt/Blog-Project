dbb89ffb26a8c5017893ab6c0ccd8d1f4d4987ec
Tanmoy Sarkar <ts741127@gmail.com>
2021-06-20 17:41:10 +0530

    Update

--- normaluser/views.py
+++ normaluser/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def homepage(request,titile):
+    print("HI")
+    print(titile)
+    return render(request,"normaluser/app.html",{"titile":titile,"name":"tanmoy Sarkar"})
